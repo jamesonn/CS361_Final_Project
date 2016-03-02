a1d88092b2d166e9cb804c86a9c057ce7506e239
Nate <jamesonn@uwm.edu>
2016-03-01 19:18:03 -0600

    Timer class added working on it more tonight 3/1

---
+++ Sprint0/src/Timer.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+
+//Unfinished, working on it more tonight 3/1/2016
+public class Timer {
+	ArrayList<Racer> Racers = new ArrayList<Racer>();
+	int lane = 0;
+	int hour;
+	int minute;
+	double second;
+	double systemTime;
+
+	public void setTime(int hour, int minute, double second){
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+		systemTime = (((hour*60)+minute)*60)+second;
+	}
+	
+	public void addNum(int racerNum){
+		Racers.add(new Racer(racerNum, lane));
+		if(lane > 4){
+			lane = 0;
+		}
+		lane++;
+	}
+	
+	public void start(int laneNum){
+		for(int i = 0; i < Racers.size(); i++){
+			Racer racer = Racers.get(i);
+			if(racer.laneNum == laneNum){
+				racer.startTime = systemTime;				
+			}
+		}		
+	}
+	
+	public void stop(int laneNum){
+		for(int i = 0; i < Racers.size(); i++){
+			Racer racer = Racers.get(i);
+			if(racer.laneNum == laneNum){
+				racer.stopTime = systemTime;
+			}
+		}
+	}
+	
+	public void calculateTime(){}
+}
