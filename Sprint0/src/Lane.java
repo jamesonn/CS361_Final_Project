445e77f87b1e927ff60a08d1bc13a08bf3f46bd1
AndreasJess <andreasj@uwm.edu>
2016-03-04 21:47:47 -0600

    last minute

--- Sprint0/src/Lane.java
+++ Sprint0/src/Lane.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList;
 
+import sun.misc.Queue;
+
 /**
  * Determines is a lane has started
  * @author Group 1
  */
 public class Lane {
-	boolean hasStarted = false;
+	private boolean hasStarted = false;
+	private Queue<Racer> racers = new Queue<Racer>();
+	private Racer currentRacer;
 	//ArrayList<Racer> racers = new ArrayList<Racer>(); //potential queue situation
 	
 	public void setStarted(boolean started){
@@ -13,11 +17,55 @@ public class Lane {
 	}
 	
 	//remove once racers are stored in the lane we the trigger starting sets them in
+	/**
+	 * 
+	 * @return boolean
+	 */
 	public boolean hasStarted(){
 		return hasStarted;
 	}
 	
-	/*public void addRacer(Racer racer){
-		racers.add(racer);
-	}*/
+	/**
+	 * dequeues the FI racer and sets their start time
+	 * @param time
+	 */
+	public void startRacer(float time){
+		currentRacer = popRacer();
+		currentRacer.setStartTime(time);
+		hasStarted = true;
+	}
+	
+	/**
+	 * 
+	 * @param time
+	 * @return
+	 */
+	public Racer racerFinished(float time){
+		currentRacer.setLapTime(time);
+		return currentRacer;
+	}
+	
+	
+	/**
+	 * adds a racer to the queue
+	 * @param racer
+	 */
+	public void pushRacer(Racer racer){
+		racers.enqueue(racer);
+	}
+	
+	/**
+	 * handles the dequeue try-catch
+	 * @return FI racer
+	 */
+	public Racer popRacer(){
+		Racer finisher = new Racer(0, 0);
+		try {
+			finisher = racers.dequeue();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return finisher;
+	}
 }
--- Sprint0/src/Timer.java
+++ Sprint0/src/Timer.java
@@ -9,17 +9,15 @@ import java.util.ArrayList;
  * @author Group 1
  */
 public class Timer {
-	ArrayList<Racer> Racers = new ArrayList<Racer>();
-	Lane lane;
+	//ArrayList<Racer> Racers = new ArrayList<Racer>();
+	//Lane lane;
 	Lane[] lanes = new Lane[4];
 	int lanesUsed;
 	int hour;
 	int minute;
 	float second;
 	float systemTime;
-	boolean laneStarted = false;
-	//int racersQueued;
-	
+	//boolean laneStarted = false;
 	
 	public Timer(){
 		lanes[0] = new Lane();
@@ -46,16 +44,11 @@ public class Timer {
 	 * @param racerNum
 	 */
 	public void addNum(int racerNum){
-		//this is startement will go
 		if(lanesUsed > 4){
 			lanesUsed = 0;
-			Racers.add(new Racer(racerNum,lanesUsed)); //LanesUsed is just the laneNum
-		}else{
-			Racers.add(new Racer(racerNum,lanesUsed));
 		}
-		//so will this
+		lanes[lanesUsed].pushRacer(new Racer(racerNum,lanesUsed));
 		lanesUsed++;
-		//new racer is created, and is marked as to the order in which is was added
 	}
 	
 	/**
@@ -63,29 +56,7 @@ public class Timer {
 	 * @param channel
 	 */
 	public void start(int channel){
-		int workingLane = 0;
-		for(int i = 0; i < Racers.size(); i++){
-			Racer racer = Racers.get(i);
-			//will no longer have to check whether the channel is a start or finish since the check is done in the switch case
-			if(channel == 1 || channel == 2){
-				workingLane = 0;
-			}
-			if(channel == 3 || channel == 4){
-				workingLane = 1;
-			}
-			if(channel == 5 || channel == 6){
-				workingLane = 2;
-			}
-			if(channel == 7 || channel == 28){
-				workingLane = 3;
-			}
-			//depending on which sensor was hit, we will place the next queued racer into the lane to which the sensor belongs
-			if(racer.laneNum == workingLane){
-				System.out.println("systime at start "+second);
-				racer.startTime = systemTime;
-				lanes[workingLane].setStarted(true);
-			}
-		}		
+		lanes[channel-1].startRacer(systemTime);	
 	}
 	
 	/**
@@ -93,59 +64,44 @@ public class Timer {
 	 * @param channel
 	 */
 	public String stop(int channel){
-		int workingLane = 0;
-		for(int i = 0; i < Racers.size(); i++){
-			Racer racer = Racers.get(i);
-			if(channel == 1 || channel == 2){
-				workingLane = 0;
-			}
-			if(channel == 3 || channel == 4){
-				workingLane = 1;
-			}
-			if(channel == 5 || channel == 6){
-				workingLane = 2;
-			}
-			if(channel == 7 || channel == 28){
-				workingLane = 3;
-			}
-			if(racer.laneNum == workingLane){
-				racer.stopTime = systemTime;
-				lanes[workingLane].setStarted(false);
-				return ""+racer.racerNum+" "+racer.getTotalTime();
-			}
+		if(lanes[channel-1].hasStarted()){
+			Racer fini = lanes[channel-1].racerFinished(systemTime);
+			return ""+fini.racerNum+" "+fini.totalTime;
 		}
+		
+		
 		return "0.0\n";
 	}
 	
-	/**
-	 * determines if the lane corresponding with the 
-	 * passed channel has started or not
-	 * @param channel
-	 * @return boolean
-	 */
-	public boolean hasLaneStarted(int channel){
-		if(channel == 1 || channel == 2){
-			if(lanes[0].hasStarted){
-				return true;
-			}
-		}
-		if(channel == 3 || channel == 4){
-			if(lanes[1].hasStarted){
-				return true;
-			}
-		}
-		if(channel == 5 || channel == 6){
-			if(lanes[2].hasStarted){
-				return true;
-			}
-		}
-		if(channel == 7 || channel == 8){
-			if(lanes[3].hasStarted){
-				return true;
-			}
-		}
-		return false;
-	}
+//	/**
+//	 * determines if the lane corresponding with the 
+//	 * passed channel has started or not
+//	 * @param channel
+//	 * @return boolean
+//	 */
+//	public boolean hasLaneStarted(int channel){
+//		if(channel == 1 || channel == 2){
+//			if(lanes[0].hasStarted){
+//				return true;
+//			}
+//		}
+//		if(channel == 3 || channel == 4){
+//			if(lanes[1].hasStarted){
+//				return true;
+//			}
+//		}
+//		if(channel == 5 || channel == 6){
+//			if(lanes[2].hasStarted){
+//				return true;
+//			}
+//		}
+//		if(channel == 7 || channel == 8){
+//			if(lanes[3].hasStarted){
+//				return true;
+//			}
+//		}
+//		return false;
+//	}
 	/**
 	 * calculates the time for...
 	 */
