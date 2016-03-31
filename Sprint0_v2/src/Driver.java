a267896ccce2035dc8d28176cf95cbd9b69fb2c8
sternsd <sternsd@uwm.edu>
2016-03-31 12:38:34 -0500

    try-catch in Driver

--- Sprint0_v2/src/Driver.java
+++ Sprint0_v2/src/Driver.java
@@ -18,7 +18,7 @@ public class Driver {
 		ArrayList<String> instructionLines = new ArrayList<String>();
 
 		try{
-			instructions = new File("Sprint2TestData2Fixed.txt");
+			instructions = new File("Sprint2TestData.txt");
 			instructionParser = new Scanner(instructions);
 			while(instructionParser.hasNextLine()){
 				instructionLines.add(instructionParser.nextLine());
@@ -27,16 +27,22 @@ public class Driver {
 		}catch(FileNotFoundException e1){}
 
 		for(int i = 0; i < instructionLines.size(); i++){
-			String[] timeCommand = instructionLines.get(i).split("\t");
-			String[] time = timeCommand[0].split(":");
-			String[] commands = timeCommand[1].split(" ");
-			hours = Integer.parseInt(time[0]);
-			minutes = Integer.parseInt(time[1]);
-			seconds = Double.parseDouble(time[2]);
-			TotalTime = seconds + minutes*60 + hours*3600;
-			SysTime = ""+hours+":"+minutes+":"+seconds+" ";
+			try{
+				String[] timeCommand = instructionLines.get(i).split("\t");
+				String[] time = timeCommand[0].split(":");
+				String[] commands = timeCommand[1].split(" ");
+				hours = Integer.parseInt(time[0]);
+				minutes = Integer.parseInt(time[1]);
+				seconds = Double.parseDouble(time[2]);
+				TotalTime = seconds + minutes*60 + hours*3600;
+				SysTime = ""+hours+":"+minutes+":"+seconds+" ";
 
-			cTimer.executeCommand(commands, TotalTime, SysTime);
+				cTimer.executeCommand(commands, TotalTime, SysTime);
+			}
+			catch(Exception e){
+
+			}
 		}
 	}
 }
+
