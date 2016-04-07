d8b6d36ed02d1df2b4661f6ca3b4832ff07032e6
Nate <jamesonn@uwm.edu>
2016-04-07 12:48:40 -0500

    now have a choice between testdata and ui

---
+++ Sprint3/src/Client.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by Nate on 4/7/2016.
+ */
+public class Client {
+    public Client(boolean formatChoice) {
+
+        ChronoTimer cTimer = new ChronoTimer();
+
+        if(formatChoice){
+            UserInterface ui = new UserInterface();
+        }else {
+            int hours;
+            int minutes;
+            double seconds;
+            double TotalTime;
+            String SysTime;
+
+            File instructions;
+            Scanner instructionParser;
+            ArrayList<String> instructionLines = new ArrayList<String>();
+
+            try {
+                instructions = new File("Sprint2TestData.txt");
+                instructionParser = new Scanner(instructions);
+                while (instructionParser.hasNextLine()) {
+                    instructionLines.add(instructionParser.nextLine());
+                }
+                instructionParser.close();
+            } catch (FileNotFoundException e1) {
+            }
+
+            for (int i = 0; i < instructionLines.size(); i++) {
+                try {
+                    String[] timeCommand = instructionLines.get(i).split("\t");
+                    String[] time = timeCommand[0].split(":");
+                    String[] commands = timeCommand[1].split(" ");
+                    hours = Integer.parseInt(time[0]);
+                    minutes = Integer.parseInt(time[1]);
+                    seconds = Double.parseDouble(time[2]);
+                    TotalTime = seconds + minutes * 60 + hours * 3600;
+                    SysTime = "" + hours + ":" + minutes + ":" + seconds + " ";
+
+                    cTimer.executeCommand(commands, TotalTime, SysTime);
+                } catch (Exception e) {
+
+                }
+            }
+        }
+    }
+}
