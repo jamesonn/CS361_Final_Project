14be20109a443cd286837dbfa8afbcc0cfb42534
Nate <jamesonn@uwm.edu>
2016-04-29 12:41:06 -0500

    Comparators for the website

---
+++ Sprint4/web/RacerNumberComparator.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+/**
+ * Created by Nate on 4/29/2016.
+ */
+public class RacerNumberComparator implements Comparator<Object> {
+
+    @Override
+    public int compare(Object o1,Object o2){
+
+        return 0;
+    }
+}
---
+++ Sprint4/web/RacerTimeComparator.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+/**
+ * Created by Nate on 4/29/2016.
+ */
+public class RacerTimeComparator implements Comparator<Object> {
+
+    @Override
+    public int compare(Object o1,Object o2){
+
+        return 0;
+    }
+}
